330c9e3bc8b155a9e61417515c350c8c2d699219
swindlesmccoop <swindlesmccoop@waifu.club>
2022-05-27 10:17:47 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Swindles McCoop's Auto Rice Tool
+Run `sh smart.sh` and follow the directions
---
+++ smart.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+command -v git > /dev/null || $(printf ""$(echo "\033[31m")"This program requires git in order to run!"$(echo "[0m")"\n" && exit)
+command -v pacman > /dev/null || $(printf ""$(echo "\033[31m")"This program requires you to be using an Arch-based distribution in order to run!"$(echo "[0m")"\n" && exit)
+
+compile_from_source () {
+	cd /tmp
+	git clone https://github.com/LukeSmithXYZ/dmenu
+	git clone https://github.com/LukeSmithXYZ/st
+	git clone https://git.cbps.xyz/swindlesmccoop/PKGBUILDS
+
+	cd dmenu && sudo make clean install && cd ..
+	cd st && sudo make clean install && cd ..
+
+	cd PKGBUILDS/VGMStream
+	makepkg -si
+	cd -
+}
+
+get_paru () {
+	sudo pacman -S --needed base-devel
+	git clone https://aur.archlinux.org/paru.git
+	cd paru
+	makepkg -si
+}
+
+get_deps () {
+	paru -Sy
+	paru -S --needed < packages.txt
+	pip install i3ipc ueberzug
+}
+
+bootstrap () {
+	echo "Any programs that have configuration files in my repo will overwrite any existing ones in ~/.config."
+	read -r -p "Continue? (y/n): " CONT
+	[ "$CONT" = y ] || $(echo "Invalid input or user has terminated!" && exit)
+
+	cd /tmp
+	git clone https://git.cbps.xyz/swindlesmccoop/not-just-dotfiles --recurse-submodules
+	cd not-just-dotfiles
+	cp -r .config/* "$HOME/.config/"
+	cp -r .local/* "$HOME/.local/"
+	sudo cp .zprofile /etc/zsh/zshenv || echo "You do not have sudo permission! Skipping zsh env."
+}
